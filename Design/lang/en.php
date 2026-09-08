fa93d880905a824a0114e0ca80d9e36a1a7fcbd0
MilanBos24 <serp24.cz@gmail.com>
2026-09-08 23:00:36 +0200

    Add English translations

---
+++ Design/lang/en.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'meta.title' => 'RAC',
+    'meta.description' => 'Books and audiobooks',
+    'menu.home' => 'Home',
+    'menu.books' => 'Books',
+    'menu.audiobooks' => 'Audiobooks',
+    'menu.contact' => 'Contact',
+    'action.search' => 'Search',
+    'cart.title' => 'Cart',
+    'action.add_to_cart' => 'Add to cart',
+];

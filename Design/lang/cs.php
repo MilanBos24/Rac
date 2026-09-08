e50d605e495086e733f0acd73a75bcb88f7bc840
MilanBos24 <serp24.cz@gmail.com>
2026-09-08 23:00:27 +0200

    Add Czech translations

---
+++ Design/lang/cs.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'meta.title' => 'RAC',
+    'meta.description' => 'Knihy a audioknihy',
+    'menu.home' => 'Domů',
+    'menu.books' => 'Knihy',
+    'menu.audiobooks' => 'Audioknihy',
+    'menu.contact' => 'Kontakt',
+    'action.search' => 'Hledat',
+    'cart.title' => 'Košík',
+    'action.add_to_cart' => 'Přidat do košíku',
+];

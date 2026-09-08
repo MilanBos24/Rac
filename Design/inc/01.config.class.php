cabd76f2d24172d7f48c7d2e7aa8d388b2179c48
MilanBos24 <serp24.cz@gmail.com>
2026-09-08 23:00:09 +0200

    Add Design language config

---
+++ Design/inc/01.config.class.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+if (!defined('BASE_PATH')) {
+    define('BASE_PATH', dirname(__DIR__));
+}
+
+if (!defined('DEFAULT_LANGUAGE')) {
+    define('DEFAULT_LANGUAGE', 'cs');
+}
+
+if (!defined('SUPPORTED_LANGUAGES')) {
+    define('SUPPORTED_LANGUAGES', ['cs', 'en']);
+}

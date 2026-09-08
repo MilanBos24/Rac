91b34a370fe832643522689ccb0b82fc5efed22f
MilanBos24 <serp24.cz@gmail.com>
2026-09-08 23:17:43 +0200

    Set Czech primary navigation labels

--- Design/lang/cs.php
+++ Design/lang/cs.php
@@ -3,11 +3,15 @@
 return [
     'meta.title' => 'RAC',
     'meta.description' => 'Knihy a audioknihy',
-    'menu.home' => 'Domů',
+
+    'menu.home' => 'Úvod',
+    'menu.author' => 'O autorovi',
     'menu.books' => 'Knihy',
-    'menu.audiobooks' => 'Audioknihy',
+    'menu.upcoming' => 'Připravujeme',
     'menu.contact' => 'Kontakt',
+
     'action.search' => 'Hledat',
     'cart.title' => 'Košík',
     'action.add_to_cart' => 'Přidat do košíku',
+    'language.label' => 'Jazyk',
 ];

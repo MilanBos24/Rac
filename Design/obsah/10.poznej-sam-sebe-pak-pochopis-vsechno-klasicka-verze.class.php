6a46ffc719ccd3406c0556ddbc61c133cad2fd91
MilanBos24 <serp24.cz@gmail.com>
2026-09-11 16:13:58 +0200

    Make book detail page language-aware

--- Design/obsah/10.poznej-sam-sebe-pak-pochopis-vsechno-klasicka-verze.class.php
+++ Design/obsah/10.poznej-sam-sebe-pak-pochopis-vsechno-klasicka-verze.class.php
@@ -3,21 +3,19 @@
         </div><!-- /.stricky-header -->
         <section class="page-header">
             <div class="page-header__bg"></div>
-            <!-- /.page-header__bg -->
             <div class="page-header__overlay"></div>
-            <!-- /.page-header__bg -->
             <div class="container">
                 <ul class="page-header__breadcrumb list-unstyled">
-                    <li><a href="/">Home</a></li>
-                    <li><span>Portfólio</span></li>
-                </ul><!-- /.page-breadcrumb list-unstyled -->
-                <h2 class="page-header__title">Poznej sám sebe. Pak pochopíš všechno.</h2><!-- /.page-title -->
-            </div><!-- /.container -->
-        </section><!-- /.page-header -->
-        <!-- Projects Details Start -->
+                    <li><a href="<?= htmlspecialchars(racUrl('index.php'), ENT_QUOTES, 'UTF-8'); ?>"><?= htmlspecialchars(__('breadcrumb.home'), ENT_QUOTES, 'UTF-8'); ?></a></li>
+                    <li><span><?= htmlspecialchars(__('breadcrumb.portfolio'), ENT_QUOTES, 'UTF-8'); ?></span></li>
+                </ul>
+                <h2 class="page-header__title">Poznej sám sebe. Pak pochopíš všechno.</h2>
+            </div>
+        </section>
+
         <section class="project-details">
             <div class="container">
-                <div class="project-details__image wow fadeInUp animated" data-wow-delay="200ms"><img src="assets/images/project/klasicka-verze.jpg" alt="ogency"></div><!-- /.project-image -->
+                <div class="project-details__image wow fadeInUp animated" data-wow-delay="200ms"><img src="assets/images/project/klasicka-verze.jpg" alt="Poznej sám sebe. Pak pochopíš všechno."></div>
                 <div class="row">
                     <div class="col-xl-8 col-lg-7 wow fadeInLeft animated" data-wow-delay="300ms">
                         <div class="project-details__content">
@@ -29,13 +27,13 @@
                                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
                             </h4>
                             <p class="project-details__content__text">
-                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vitae arcu tempor neque lacinia pretium. Vivamus porttitor turpis ac leo. Suspendisse nisl. Vivamus ac leo pretium faucibus. Mauris suscipit, 
-                                ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Fusce tellus odio, dapibus id fermentum quis, suscipit id erat. 
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vitae arcu tempor neque lacinia pretium. Vivamus porttitor turpis ac leo. Suspendisse nisl. Vivamus ac leo pretium faucibus. Mauris suscipit,
+                                ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Fusce tellus odio, dapibus id fermentum quis, suscipit id erat.
                                 Curabitur bibendum justo non orci. Quisque porta. Integer tempor. Nulla est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis viverra diam non justo.
                             </p>
                             <p class="project-details__content__text">
-                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vitae arcu tempor neque lacinia pretium. Vivamus porttitor turpis ac leo. Suspendisse nisl. Vivamus ac leo pretium faucibus. Mauris suscipit, 
-                                ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Fusce tellus odio, dapibus id fermentum quis, suscipit id erat. 
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vitae arcu tempor neque lacinia pretium. Vivamus porttitor turpis ac leo. Suspendisse nisl. Vivamus ac leo pretium faucibus. Mauris suscipit,
+                                ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Fusce tellus odio, dapibus id fermentum quis, suscipit id erat.
                                 Curabitur bibendum justo non orci. Quisque porta. Integer tempor. Nulla est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis viverra diam non justo.
                             </p>
                             <h4 class="project-details__content__title-three">Praesent vitae arcu</h4>
@@ -45,7 +43,7 @@
                                 <li><span class="fa fa-check"></span>Accusamus et iusto odio dignissimos ducimus</li>
                             </ul>
                             <p class="project-details__content__text">
-                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vitae arcu tempor neque lacinia pretium. Vivamus porttitor turpis ac leo. Suspendisse nisl. Vivamus ac leo pretium faucibus. Mauris suscipit, 
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vitae arcu tempor neque lacinia pretium. Vivamus porttitor turpis ac leo. Suspendisse nisl. Vivamus ac leo pretium faucibus. Mauris suscipit,
                                 ligula sit amet pharetra semper
                             </p>
                         </div>
@@ -53,9 +51,9 @@
                     <div class="col-xl-4 col-lg-5 wow fadeInRight animated" data-wow-delay="400ms">
                         <div class="project-details__right">
                             <ul class="project-details__info-list list-unstyled">
-                                <li><span>Datum vydání:</span>????</li>
-                                <li><span>Autor:</span>Štefan Rác</li>
-                                <li><span>Verze:</span>Audiokniha</li>
+                                <li><span><?= htmlspecialchars(__('product.release_date'), ENT_QUOTES, 'UTF-8'); ?></span>????</li>
+                                <li><span><?= htmlspecialchars(__('product.author'), ENT_QUOTES, 'UTF-8'); ?></span>Štefan Rác</li>
+                                <li><span><?= htmlspecialchars(__('product.version'), ENT_QUOTES, 'UTF-8'); ?></span><?= htmlspecialchars(__('product.version.audiobook'), ENT_QUOTES, 'UTF-8'); ?></li>
                             </ul>
                             <div class="project-details__socials">
                                 <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
@@ -65,80 +63,46 @@
                             </div>
                         </div>
                     </div>
-                </div><!-- /.project-details-content -->
+                </div>
+
                 <div class="project-details__pagination wow fadeInUp animated" data-wow-delay="400ms">
-                    <a class="project-details__pagination__previous" href="#" aria-label="Předchozí"><span class="icon-left-arrow"></span>Předchozí</a>
-                    <a class="project-details__pagination__next" href="#" aria-label="Další">Další<span class="icon-right-arrow"></span></a>
-                </div><!-- /.project-pagination -->
+                    <a class="project-details__pagination__previous" href="#" aria-label="<?= htmlspecialchars(__('action.previous'), ENT_QUOTES, 'UTF-8'); ?>"><span class="icon-left-arrow"></span><?= htmlspecialchars(__('action.previous'), ENT_QUOTES, 'UTF-8'); ?></a>
+                    <a class="project-details__pagination__next" href="#" aria-label="<?= htmlspecialchars(__('action.next'), ENT_QUOTES, 'UTF-8'); ?>"><?= htmlspecialchars(__('action.next'), ENT_QUOTES, 'UTF-8'); ?><span class="icon-right-arrow"></span></a>
+                </div>
+
                 <div class="section-title text-center wow fadeInUp animated" data-wow-delay="400ms">
-                    <h5 class="section-title__tagline section-title__tagline--has-dots">moje tvorba</h5>
-                    <h2 class="section-title__title">Knihy & projekty</h2>
-                </div><!-- related-project-section-title -->
+                    <h5 class="section-title__tagline section-title__tagline--has-dots"><?= htmlspecialchars(__('section.my_work'), ENT_QUOTES, 'UTF-8'); ?></h5>
+                    <h2 class="section-title__title"><?= htmlspecialchars(__('section.books_projects'), ENT_QUOTES, 'UTF-8'); ?></h2>
+                </div>
+
+                <?php
+                $relatedBooks = [
+                    ['image' => 'klasicka.jpg', 'title' => 'Poznej sám sebe. Pak pochopíš všechno.'],
+                    ['image' => 'komunikace.jpg', 'title' => 'Poznej sám sebe. Pak pochopíš všechno. — Komunikace se sebou'],
+                    ['image' => 'rozsirena.jpg', 'title' => 'Poznej sám sebe. Pak pochopíš všechno. — Rozšířená verze'],
+                ];
+                ?>
                 <div class="row">
-  <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="200ms">
-                        <div class="blog-one__item">
-                            <div class="blog-one__item__image">
-                                <img src="assets/images/knihy/klasicka.jpg" alt="Poznej sám sebe. Pak pochopíš všechno.">
-                                <a href="#"></a>
-                                <span>v prodeji</span>
-                            </div><!-- /.blog-image -->
-                            <div class="blog-one__item__content">
-                                <h3 class="blog-one__item__title">
-                                    <a href="poznej-sam-sebe-pak-pochopis-vsechno-klasicka-verze.php">Poznej sám sebe. Pak pochopíš všechno.</a>
-                                </h3><!-- /.blog-title -->
-                          
-                                                      </div><!-- /.qty-btn -->                           
-                            <div class="product-details__buttons">
-                                <a href="poznej-sam-sebe-pak-pochopis-vsechno-klasicka-verze.php" class="ogency-btn wishlist">více o knize</a>
-                                <a href="kosik.php" class="ogency-btn">koupit</a>
+                    <?php foreach ($relatedBooks as $book): ?>
+                        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="200ms">
+                            <div class="blog-one__item">
+                                <div class="blog-one__item__image">
+                                    <img src="assets/images/knihy/<?= htmlspecialchars($book['image'], ENT_QUOTES, 'UTF-8'); ?>" alt="<?= htmlspecialchars($book['title'], ENT_QUOTES, 'UTF-8'); ?>">
+                                    <a href="<?= htmlspecialchars(racUrl('poznej-sam-sebe-pak-pochopis-vsechno-klasicka-verze.php'), ENT_QUOTES, 'UTF-8'); ?>"></a>
+                                    <span><?= htmlspecialchars(__('books.status.on_sale'), ENT_QUOTES, 'UTF-8'); ?></span>
+                                </div>
+                                <div class="blog-one__item__content">
+                                    <h3 class="blog-one__item__title">
+                                        <a href="<?= htmlspecialchars(racUrl('poznej-sam-sebe-pak-pochopis-vsechno-klasicka-verze.php'), ENT_QUOTES, 'UTF-8'); ?>"><?= htmlspecialchars($book['title'], ENT_QUOTES, 'UTF-8'); ?></a>
+                                    </h3>
+                                </div>
+                                <div class="product-details__buttons">
+                                    <a href="<?= htmlspecialchars(racUrl('poznej-sam-sebe-pak-pochopis-vsechno-klasicka-verze.php'), ENT_QUOTES, 'UTF-8'); ?>" class="ogency-btn wishlist"><?= htmlspecialchars(__('action.more_about_book'), ENT_QUOTES, 'UTF-8'); ?></a>
+                                    <a href="<?= htmlspecialchars(racUrl('kosik.php'), ENT_QUOTES, 'UTF-8'); ?>" class="ogency-btn"><?= htmlspecialchars(__('action.buy'), ENT_QUOTES, 'UTF-8'); ?></a>
+                                </div>
                             </div>
-                                                   
-                            </div><!-- /.blog-content -->
-                    </div>
-                    
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="200ms">
-                        <div class="blog-one__item">
-                            <div class="blog-one__item__image">
-                                <img src="assets/images/knihy/komunikace.jpg" alt="Poznej sám sebe. Pak pochopíš všechno.  — Komunikace se sebou">
-                                <a href="#"></a>
-                                <span>v prodeji</span>
-                            </div><!-- /.blog-image -->
-                            <div class="blog-one__item__content">
-                                <h3 class="blog-one__item__title">
-                                    <a href="poznej-sam-sebe-pak-pochopis-vsechno-klasicka-verze.php">Poznej sám sebe. Pak pochopíš všechno.  — Komunikace se sebou</a>
-                                </h3><!-- /.blog-title -->
-                          
-                                                      </div><!-- /.qty-btn -->                           
-                            <div class="product-details__buttons">
-                                <a href="poznej-sam-sebe-pak-pochopis-vsechno-klasicka-verze.php" class="ogency-btn wishlist">více o knize</a>
-                                <a href="kosik.php" class="ogency-btn">koupit</a>
-                            </div>
-                                                   
-                            </div><!-- /.blog-content -->
-                    </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="200ms">
-                        <div class="blog-one__item">
-                            <div class="blog-one__item__image">
-                                <img src="assets/images/knihy/rozsirena.jpg" alt="Poznej sám sebe. Pak pochopíš všechno. — Rozšířená verze">
-                                <a href="#"></a>
-                                <span>v prodeji</span>
-                            </div><!-- /.blog-image -->
-                            <div class="blog-one__item__content">
-                                <h3 class="blog-one__item__title">
-                                    <a href="poznej-sam-sebe-pak-pochopis-vsechno-klasicka-verze.php">Poznej sám sebe. Pak pochopíš všechno. — Rozšířená verze</a>
-                                </h3><!-- /.blog-title -->
-                          
-                                                      </div><!-- /.qty-btn -->                           
-                            <div class="product-details__buttons">
-                                <a href="poznej-sam-sebe-pak-pochopis-vsechno-klasicka-verze.php" class="ogency-btn wishlist">více o knize</a>
-                                <a href="kosik.php" class="ogency-btn">koupit</a>
-                            </div>
-                                                   
-                            </div><!-- /.blog-content -->
-                    </div>
+                        </div>
+                    <?php endforeach; ?>
                 </div>
             </div>
         </section>
-        <!-- Projects Details End -->
-
-
